e9fe034ba966515fb30f53295cc617cbe580e085
Marc Backes <marc.backes@talkevent.de>
2024-10-06 15:08:50 +0200

    v0

--- src/views/DemoView.vue
+++ src/views/DemoView.vue
@@ -9,13 +9,8 @@ const state = reactive({
   <div>age: {{ state.age }}</div>
   <div style="margin: 1rem 0; display: flex; gap: 1rem">
     <button @click="state.age++">+</button>
-    <!-- <button @click="backwards()">backwards</button>
-    <button @click="forwards()">forwards</button> -->
-    <!-- <button @click="backInTime(5)">back in time 5s</button> -->
   </div>
   <hr />
-  <!-- Index: {{ historyIndex }}
-  <pre>{{ history }}</pre> -->
 </template>
 
 <style scoped>
